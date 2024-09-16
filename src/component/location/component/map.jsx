8e41811bef1f6beee4b3b6c75d70c6387a1d69fe
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2024-09-16 14:52:45 +0700

    Fix : CDN

--- src/component/location/component/map.jsx
+++ src/component/location/component/map.jsx
@@ -1,7 +1,7 @@
 export const Map = () => {
     return (
         <div className=" flex justify-center rounded-3xl overflow-hidden w-full h-full shadow">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126907.03918565117!2d106.71967758688244!3d-6.284100103057176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1ec2422b0b3%3A0x39a0d0fe47404d02!2sJakarta%20Selatan%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1726393329971!5m2!1sid!2sid" width="1500" height="490" style={{border:0}} allowFullScreen="true" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126907.03918565117!2d106.71967758688244!3d-6.284100103057176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f1ec2422b0b3%3A0x39a0d0fe47404d02!2sJakarta%20Selatan%2C%20Kota%20Jakarta%20Selatan%2C%20Daerah%20Khusus%20Ibukota%20Jakarta!5e0!3m2!1sid!2sid!4v1726393329971!5m2!1sid!2sid" width="1500" height="490" style={{border:0}} allowFullScreen={ true } loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
     )
 }
--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -3,7 +3,7 @@ export const Navbar = () => {
     return (
         <nav className="bg-transparent absolute w-full z-20 top-0 border-gray-200 dark:border-gray-600">
             <div className="relative max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <span className="self-center text-2xl font-bold whitespace-nowrap text-white">Sedot WC</span>
                 </a>
                 <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
